7db6cdcaf982bde65bc8f7ce3b001c27301eba1d
Ural Ataullin <ataullinural@gmail.com>
2024-03-05 16:13:14 +0500

    Slug for category

--- goods/views.py
+++ goods/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Product
 
 
-def catalog(request):
-    goods = Product.objects.all()
-    data = {
-        "title": "Home - Catalog",
-        "goods": goods
-    }
+def catalog(request, category_slug):
+
+    if category_slug == "all":
+        goods = Product.objects.all()
+    else:
+        goods = get_list_or_404(Product.objects.filter(cat__slug=category_slug))
+
+    data = {"title": "Home - Catalog", "goods": goods}
     return render(request, "goods/catalog.html", data)
 
 
 def product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
-    data = {
-        'product': product
-    }
+    data = {"product": product}
     return render(request, "goods/product.html", data)
