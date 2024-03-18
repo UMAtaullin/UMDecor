fadff7f714f5f1e498b92d1ba90e1eb336c5dedb
Ural Ataullin <ataullinural@gmail.com>
2024-03-18 10:41:26 +0500

    PostgreSQL connected and full text search

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     # tools
     "debug_toolbar",
     # local apps
@@ -57,8 +58,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "umdecor",
+        "USER": "umdecor",
+        "PASSWORD": "umdecor",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
