91917e35818b7bd930def5e89802ded15240ad20
Ural Ataullin <ataullinural@gmail.com>
2024-03-20 09:12:05 +0500

    forms with label and widget

--- users/views.py
+++ users/views.py
@@ -1,9 +1,35 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request):
+    # Если это запрос POST, нам необходимо обработать данные формы.
+    if request.method == "POST":
+        # Создайте экземпляр формы и заполните его данными из запроса:
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            username = request.POST['username']
+            password = request.POST['password']
+            # auth.authenticate проверяет есть ли такой пользователь в БД.
+            user = auth.authenticate(
+                username=username, password=password)
+            if user:
+                # auth.login для авторизации пользователя.
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
 
-    data = {"title": "Home - Авторизация"}
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = UserLoginForm()
+
+    data = {
+        "title": "Home - Авторизация",
+        "form": form}
 
     return render(request, "users/login.html", data)
 
