42c934252866594573a82bc055227408b1017d21
Ural Ataullin <ataullinural@gmail.com>
2024-03-21 10:56:17 +0500

    Profile is ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 
 from users.models import User
 
@@ -13,8 +17,8 @@ class UserLoginForm(AuthenticationForm):
             "password",
         )
 
-    # username = forms.CharField()
-    # password = forms.CharField()
+    username = forms.CharField()
+    password = forms.CharField()
 
     # username = forms.CharField(
     #     label='Имя',
@@ -50,3 +54,28 @@ class UserRegistrationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -21,6 +22,7 @@ def login(request):
             if user:
                 # auth.login для авторизации пользователя.
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт.")
                 return HttpResponseRedirect(reverse("main:index"))
 
     # if a GET (or any other method) we'll create a blank form
@@ -41,6 +43,8 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(
+                request, f"{user.username}, Вы успешно зарегистрировались.")
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
@@ -53,13 +57,31 @@ def registration(request):
     return render(request, "users/registration.html", data)
 
 
+@login_required
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST,
+            instance=request.user,
+            files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно обновлен.")
+            return HttpResponseRedirect(reverse("users:profile"))
+    else:
+        form = ProfileForm(instance=request.user)
 
-    data = {"title": "Home - Кабинет"}
+    data = {
+        "title": "Home - Кабинет",
+        "form": form,
+        }
 
     return render(request, "users/profile.html", data)
 
 
+@login_required()
 def logout(request):
+    messages.success(
+        request, f"{request.user.username}, Вы вышли из аккаунта.")
     auth.logout(request)
     return redirect(reverse("main:index"))
